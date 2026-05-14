61debfb23791af2fc7e1702d2ff67a7edcaf916b
kieu2251220038 <kieu_225122003@dau.edu.vn>
2026-05-14 22:24:31 +0700

    chore(frontend): add eslint, lint step in CI and fix index config script

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'no-console': 'warn',
+  },
+};
